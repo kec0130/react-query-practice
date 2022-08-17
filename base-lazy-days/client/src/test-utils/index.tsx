cf8d4405c32351bf7df158576f42e0f659456bab
Eunchae Ko <kec0130@gmail.com>
2022-08-17 15:51:50 +0900

    Add tests for rendered query data

--- base-lazy-days/client/src/test-utils/index.tsx
+++ base-lazy-days/client/src/test-utils/index.tsx
@@ -1,8 +1,25 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
 
-// from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
+// make a function to generate a unique query client for each test
+const generateQueryClient = () => {
+  return new QueryClient();
+};
+
+export function renderWithQueryClient(
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult {
+  const queryClient = client ?? generateQueryClient();
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+}
+
+// for testing custom hooks
 // export const createQueryClientWrapper = (): React.FC => {
 //   const queryClient = generateQueryClient();
 //   return ({ children }) => (
